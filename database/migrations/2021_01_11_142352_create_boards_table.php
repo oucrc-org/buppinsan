4286b18d4d2f5869678cda12a8b019a11201b5f1
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-01-11 23:59:46 +0900

    [migration] マイグレーションを実行

---
+++ database/migrations/2021_01_11_142352_create_boards_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('tepra_number', 50);
+            $table->string('kind')->nullable();
+            $table->string('photo_path');
+            $table->string('detail', 200);
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boards');
+    }
+}
