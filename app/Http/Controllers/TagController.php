b2dbc519f43dceea4abff54d35eb4ece919bec71
smpny7 <symphonymode7@icloud.com>
2021-02-15 14:11:55 +0900

    [clean] リファクタリング

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tag;
+use Exception;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Redirector;
-use Illuminate\Support\Facades\Log;
 use Illuminate\View\View;
 
 class TagController extends Controller
@@ -40,11 +40,12 @@ class TagController extends Controller
      * Store a newly created resource in storage.
      *
      * @param Request $request
+     * @return Application|RedirectResponse|Redirector
      */
     public function store(Request $request)
     {
         Tag::query()->insert(['name' => $request->tag_name]);
-        return redirect('/tag');
+        return redirect(route('tag.index'));
     }
 
     /**
@@ -74,22 +75,24 @@ class TagController extends Controller
      *
      * @param Request $request
      * @param  int  $id
-     * @return Response
+     * @return Application|RedirectResponse|Response|Redirector
      */
     public function update(Request $request, $id)
     {
-        Tag::where('id',$id)->update(['name' => $request->tag_edit]);
-        return redirect('/tag');
+        Tag::query()->where('id',$id)->update(['name' => $request->tag_edit]);
+        return redirect(route('tag.index'));
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param int $id
+     * @return Application|RedirectResponse|Redirector
+     * @throws Exception
      */
     public function destroy(int $id)
     {
-        Tag::find($id)->delete();
-        return redirect('/tag');
+        Tag::query()->find($id)->delete();
+        return redirect(route('tag.index'));
     }
 }
