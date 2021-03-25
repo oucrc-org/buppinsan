9581ba63f8b7d689714876bd45501b22f5fd3b50
K.Ikeda <58544849+smpny7@users.noreply.github.com>
2021-03-25 17:23:08 +0900

    [add] Material UI 導入

--- resources/js/components/Aside.js
+++ resources/js/components/Aside.js
@@ -1,19 +1,50 @@
-import {Link} from 'react-router-dom';
+import React from 'react';
+import {Drawer, IconButton, ListItem, ListItemIcon, ListItemText} from "@material-ui/core";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
 
-function Aside() {
+export default function Aside(props) {
+    const classes = props.classes;
+    const theme = props.theme;
+    const handleDrawerClose = props.handleDrawerClose;
+
     return (
-        <aside className='aside'>
-            <Link to='/'>一覧を表示</Link>
-            <br/>
-            <Link to='/board'>物品一覧（仮）</Link>
-            <p className="bg-primary my-3">タグ</p>
-
-            <p className="bg-primary my-3">その他</p>
-            <Link to='/tag'>タグ管理</Link>
-            <br/>
-            <Link to='/user'>ユーザ管理</Link>
-        </aside>
+        <Drawer
+            className={classes.drawer}
+            variant="persistent"
+            anchor="left"
+            open={props.open}
+            classes={{
+                paper: classes.drawerPaper,
+            }}
+        >
+            <div className={classes.drawerHeader}>
+                <IconButton onClick={handleDrawerClose}>
+                    {theme.direction === 'ltr' ? <ChevronLeftIcon/> : <ChevronRightIcon/>}
+                </IconButton>
+            </div>
+            <Divider/>
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
+                        <ListItemText primary={text}/>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider/>
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
+                        <ListItemText primary={text}/>
+                    </ListItem>
+                ))}
+            </List>
+        </Drawer>
     );
 }
-
-export default Aside;
